1954faaa1e4aefcaccf5d102e9f93f71e29bead6
Rahul Dev E <rahul.e.dev@gmail.com>
2023-12-23 11:12:10 -0500

    use mse loss for mol descriptors

--- molT/masked_molecular_modelling/mol_descriptors.py
+++ molT/masked_molecular_modelling/mol_descriptors.py
@@ -33,8 +33,8 @@ class MolDescriptorModellingHead(nn.Module):
         # we do this by zeroing out rmse error based on final_mask
         mol_desc_token_mask = token_type_ids == TokenType.DESC
         final_mask = mol_desc_token_mask & mm_mask
-        rmse_error = torch.norm(preds - mol_descriptors, 2)
+        mse_error = torch.pow(preds - mol_descriptors, 2)
         # zero out any token not in final_mask
-        rmse_error = rmse_error.masked_fill(~final_mask, 0.0)
-        loss = rmse_error.mean()
-        return loss, preds
+        mse_error = mse_error.masked_fill(~final_mask, 0.0)
+        loss = mse_error.mean()
+        return loss, preds.masked_fill(~mol_desc_token_mask, 0.0)

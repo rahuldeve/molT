d0f9a27d5d041cf780116c486986e3f50e35ca4b
Rahul Dev E <rahul.e.dev@gmail.com>
2023-12-28 18:22:21 -0500

    drop bias and non linear layer in modellinghead

--- molT/masked_molecular_modelling/base.py
+++ molT/masked_molecular_modelling/base.py
@@ -13,25 +13,14 @@ class ModellingHead(nn.Module):
     def __init__(self, out_size, config):
         super().__init__()
         self.out_size = out_size
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
 
-        self.decoder = nn.Linear(config.hidden_size, out_size)
-        self.bias = nn.Parameter(torch.zeros(out_size))
-        self.decoder.bias = self.bias
-
-    def _tie_weights(self):
-        # To tie those two weights if they get disconnected (on TPU or when the bias is resized)
-        # For accelerate compatibility and to not break backward compatibility
-        if self.decoder.bias.device.type == "meta":
-            self.decoder.bias = self.bias
-        else:
-            self.bias = self.decoder.bias
+        self.decoder = nn.Linear(config.hidden_size, out_size, bias=False)
 
     def forward(self, features):
-        x = self.dense(features)
-        x = gelu(x)
-        x = self.layer_norm(x)
+        # x = self.dense(features)
+        x = self.layer_norm(features)
 
         # project back to size of vocabulary with bias
         x = self.decoder(x)

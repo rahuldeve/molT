Fix y_true and y_pred calculation for xval and cls types

molT/masked_molecular_modelling/target.py
@@ -41,4 +41,4 @@ class TargetModellingHead(nn.Module):
         # zero out any token not in final_mask
         mse_error = mse_error.masked_fill(~final_mask, 0.0)
         loss = mse_error.mean(dim=0).sum()
-        return loss, preds.masked_fill(~target_token_mask, 0.0)
+        return loss, preds[target_token_mask], target_values[target_token_mask]

molT/regression/cls_type.py
@@ -43,7 +43,7 @@ class CLSTargetModellingHead(nn.Module):
         target_token_mask = token_type_ids == TokenType.TGT
         target_values = target_values[target_token_mask]
         loss = nn.functional.mse_loss(preds, target_values)
-        return loss, preds
+        return loss, preds, target_values
 
 
 
@@ -119,7 +119,7 @@ class CLSRegression(MolTPreTrainedModel):
         )
 
         sequence_output = outputs[0]
-        target_loss, pred_target_values = self.target_head(
+        target_loss, pred_target_values, target_values = self.target_head(
             sequence_output, target_values, mm_mask, token_type_ids
         )
 
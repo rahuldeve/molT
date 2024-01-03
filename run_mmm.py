c01e41de8c7d0a1bc7d46624a1bbd3ddd3762338
Rahul Dev E <rahul.e.dev@gmail.com>
2024-01-02 23:54:15 -0500

    update run configs

--- run_mmm.py
+++ run_mmm.py
@@ -14,6 +14,7 @@ from molT import (
 )
 
 os.environ["WANDB_PROJECT"] = "molt"
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"
 
 
 def tokenize(entry, tokenizer):
@@ -36,11 +37,11 @@ def train_func(model, ds, data_collator):
         num_train_epochs=4,
         weight_decay=0.01,
         push_to_hub=False,
-        logging_steps=16,
-        eval_steps=64,
-        per_device_train_batch_size=512,
-        per_device_eval_batch_size=512,
-        gradient_accumulation_steps=4,
+        logging_steps=8,
+        eval_steps=32,
+        per_device_train_batch_size=256,
+        per_device_eval_batch_size=256,
+        gradient_accumulation_steps=8,
         warmup_ratio=0.1,
         report_to="wandb",
         dataloader_num_workers=16,
@@ -50,6 +51,9 @@ def train_func(model, ds, data_collator):
         dataloader_pin_memory=True,
         dataloader_drop_last=True,
         max_grad_norm=0.5,
+        save_steps=128,
+        save_strategy="steps",
+        load_best_model_at_end=True
     )
 
     trainer = Trainer(
@@ -72,12 +76,12 @@ if __name__ == "__main__":
 
     ds = (
         load_dataset("sagawa/ZINC-canonicalized")["validation"]
-        .select(range(1_000_000))
+        .select(range(500_000))
         .train_test_split(seed=42)
     )
 
     ds, _ = generate_and_scale_mol_descriptors(
-        ds, model_config.mol_descriptors, num_samples=500_000, num_proc=32
+        ds, model_config.mol_descriptors, num_samples=250_000, num_proc=32
     )
 
     tok_func = partial(tokenize, tokenizer=tokenizer)

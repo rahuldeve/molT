3a0bd1b260426fe803b2632d613a5630b57df8fe
Rahul Dev E <rahul.e.dev@gmail.com>
2024-01-15 14:34:23 -0500

    use simple linear head for cls target

--- molT/modelling_heads/cls_head.py
+++ molT/modelling_heads/cls_head.py
@@ -7,12 +7,12 @@ from .base import ExpDive, ModellingHead
 class CLSTargetRegressionHead(nn.Module):
     def __init__(self, config) -> None:
         super().__init__()
-        self.projection = ModellingHead(1, config)
+        self.linear = nn.Linear(config.hidden_size, 1, bias=False)
         self.exp_dive = ExpDive()
 
     def forward(self, features, target_values, mm_mask, token_type_ids):
-        preds = self.projection(features[:, 0, :])
-        preds = self.exp_dive(preds).squeeze()
+        features = self.linear(features[:, 0, :])
+        preds = self.exp_dive(features).squeeze()
 
         # calculate loss only for tokens that are mol descriptors and have been masked
         # we do this by zeroing out rmse error based on final_mask
